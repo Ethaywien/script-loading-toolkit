4aca41b14c4e1edd654dab5d12bfe868b7808735
Ethaywien <hello@alfrick.com.au>
2019-05-04 17:03:43 +1000

    Change exports to exclude Builder functions

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from './FunctionQueue';
-export * from './BasicScript';
-export * from './Script';
+export { FunctionQueue, FunctionQueueMixin, FunctionQueueState, QueueableFunction, QueuedFunction, initialFunctionQueueState } from './FunctionQueue';
+export { BasicScript, BasicScriptMixin, BasicScriptState, initialBasicScriptState } from './BasicScript';
+export { Script, ScriptInitializerMixin, ScriptMixin, ScriptState, initialScriptState } from './Script';
 export { loadScript } from './utilities/loadScript';
