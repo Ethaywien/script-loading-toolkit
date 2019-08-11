513382ed85a6d4f91ee092cd81f6714bb93b9705
Ethaywien <hello@alfrick.com.au>
2019-08-11 20:52:59 +1000

    Fix unit tests for loadScript

--- src/utilities/loadScript.spec.ts
+++ src/utilities/loadScript.spec.ts
@@ -1,23 +1,31 @@
+import * as sinon from 'sinon';
 import { loadScript } from './loadScript';
 import { testSources } from  '../../test/globals';
 
 const { validSrc, notFoundSrc } = testSources;
 
 describe('loadScript', (): void => {
+    let documentHeadStub: sinon.SinonStub;
     beforeEach((): void => {
         window.testScriptLoaded = false;
         document.head.innerHTML = '';
+        documentHeadStub = sinon.stub(document.head, 'appendChild').callsFake((script: Node): Node => {
+            // @ts-ignore: Suprress mock implementation error
+            script.onload();
+            window.testScriptLoaded = true;
+            return script;
+        })
+    });
+    afterEach((): void => {
+        documentHeadStub.restore();
     });
     it('Adds a new script tag with the given source to the page.', (): void => {
         loadScript(validSrc);
-        const scripts = document.getElementsByTagName('script');
-        expect(scripts.length).toBe(1);
-        expect(scripts[0].src).toBe(validSrc);
+        expect(documentHeadStub.args[0][0].src).toBe(validSrc);
     });
     it('Adds the script tag with type of text/javascript.', (): void => {
         loadScript(validSrc);
-        const scripts = document.getElementsByTagName('script');
-        expect(scripts[0].type).toBe('text/javascript');
+        expect(documentHeadStub.args[0][0].type).toBe('text/javascript');
     });
     it('Resolves when the script has loaded.', async (): Promise<void> => {
         await loadScript(validSrc);
@@ -29,6 +37,12 @@ describe('loadScript', (): void => {
                 setTimeout((): void => this.onerror(new Error('error')));
                 return source;
             }
+        });
+        documentHeadStub.restore();
+        documentHeadStub = sinon.stub(document.head, 'appendChild').callsFake((script: Node): Node => {
+            // @ts-ignore: Suprress mock implementation error
+            script.onerror(new Error('error'));
+            return script;
         });
         expect.assertions(2);
         try {
